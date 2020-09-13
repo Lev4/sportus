672e8e433b3c3d6e46f8c09c510f7772581edf47
Левченко Алексей Сергеевич <Levchenko.A.Sergeevic@sberbank.ru>
2020-09-13 16:38:57 +0300

    add new account

--- cl_booker.py
+++ cl_booker.py
@@ -1,9 +1,11 @@
 from sportme import SportmeBooker
 from localconfig import logindata
 
-login = logindata['inbox']['username']
-password = logindata['inbox']['password']
+# login = logindata['inbox']['username']
+# password = logindata['inbox']['password']
 
+login = logindata['gmail']['username']
+password = logindata['gmail']['password']
 
 sp = SportmeBooker(login, password)
 
--- localconfig.py
+++ localconfig.py
@@ -1,12 +1,10 @@
-
 sportmetoken = "1376373965:AAGaApS37nIxrLaggoZ0VjOk15ynaJpaUyE"
 
 username = 'rs31@inbox.ru'
 password = 'ALEX12345'
 
-
-
 logindata = {
- 'inbox':{"username":"rs31@inbox.ru","password": "ALEX12345"},
- 'gmail': {"username":"supermariolanza@gmail.com","password":"Alex12345"}
+    'inbox': {"username": "rs31@inbox.ru", "password": "ALEX12345"},
+    'gmail': {"username": "supermariolanza@gmail.com", "password": "Alex12345"},
+    'mail': {"username": "alev4enko@mail.ru", "password": "Alex12345"},
 }
